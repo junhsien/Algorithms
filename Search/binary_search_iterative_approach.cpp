b5e38b42aabe13787e3bb68b2bf8acf8da695d51
junhsien <william201129@gmail.com>
2023-01-03 19:00:50 +0800

    Create binary_search_iterative_approach.cpp

---
+++ Search/binary_search_iterative_approach.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binarySearch(vector<int> v, int To_Find)
+{
+	int lo=0, hi = v.size()-1;
+	int mid;
+	// This below check covers all cases , so need to check
+    // for mid=lo-(hi-lo)/2
+	while(hi-lo>1)
+	{
+		int mid = (hi+lo)/2;
+		if(v[mid]<To_Find){
+			lo = mid+1;
+		}else {
+			hi=mid-1;
+		}
+	}
+	if(v[lo]==To_Find)
+		cout << "Found At Index " << lo << endl;
+	else if(v[hi] == To_Find)
+		cout << "Found At Index " << hi << endl;
+	else
+		cout << "Not Found" << endl;
+	return 0;
+}
+
+int main()
+{
+	vector<int> v = {1, 2, 3, 4, 5, 6};
+	int To_Find = 1;
+	binarySearch(v, To_Find);
+	To_Find = 6;
+	binarySearch(v, To_Find);
+	To_Find = 10;
+	binarySearch(v, To_Find);
+	
+	
+	
+}
